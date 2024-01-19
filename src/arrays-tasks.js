524e04390012fa7c99b8137d4c0a4c11f2ad93a5
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-19 19:30:36 +0200

    feat: create getAverage function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -140,8 +140,11 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const sum = arr.reduce((acc, current) => acc + current, 0);
+  if (sum === 0) return 0;
+  const result = parseFloat((sum / arr.length).toFixed(2));
+  return result;
 }
 
 /**
