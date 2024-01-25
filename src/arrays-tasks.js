c248ad759ff70282cc84d786934cdd90e7fc087e
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-25 12:13:19 +0200

    feat: create shiftArray function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -554,8 +554,15 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const shiftPosition = n % arr.length;
+  if (n < 0) {
+    return arr.slice(-shiftPosition).concat(arr.slice(0, -shiftPosition));
+  }
+  if (n > 0) {
+    return arr.slice(-shiftPosition).concat(arr.slice(0, -shiftPosition));
+  }
+  return arr.slice();
 }
 
 /**
